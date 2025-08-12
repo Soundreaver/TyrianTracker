95f68d32064ee029ab3022eb8882fdfc42c90321
Soundreaver <soundburztwave@gmail.com>
2025-08-13 00:00:34 +0600

    Updated server/index.ts

--- server/index.ts
+++ server/index.ts
@@ -50,12 +50,9 @@ app.use((req, res, next) => {
   // importantly only setup vite in development and after
   // setting up all the other routes so the catch-all route
   // doesn't interfere with the other routes
-  if (app.get("env") === "development") {
+  if (process.env.NODE_ENV === "development") {
     const { setupVite } = await import("./vite");
     await setupVite(app, server);
-  } else {
-    const { serveStatic } = await import("./vite");
-    serveStatic(app);
   }
 
   // ALWAYS serve the app on the port specified in the environment variable PORT
